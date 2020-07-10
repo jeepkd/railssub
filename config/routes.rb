0943f13f42c51c879cd2c68111ccbb09c94e89d8
Jeep Kiddee <nutthawut.ki@gmail.com>
2020-07-10 17:19:37 +0700

    Create sites

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  resources :accounts
+
+  constraints(subdomain: /.+/) do
+    # root to: 'accounts#show'
+    get '/', to: 'accounts#show'
+  end
+  root to: 'pages#home'
+
 end
